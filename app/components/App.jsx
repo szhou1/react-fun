cd084c0b4c8c265cd087bd5a65f2eaf84c8238fc
szhou1 <zhousteve3@gmail.com>
2016-11-26 17:16:56 -0800

    Add lifecycle methods

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -3,8 +3,69 @@ import ReactDOM from 'react-dom';
 
 class App extends React.Component {
 
+  constructor(props) {
+    super();
+    console.log('constructor()');
+    this.state = {
+      name: 'Steve Zhou',
+      age: 29
+    }
+  }
+
+  componentWillMount() {
+    console.log('componentWillMount()');
+
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount()');
+  }
+
+  componentWillReceiveProps(nextProps) {
+    console.log('componentWillReceiveProps()', nextProps);
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('shouldComponentUpdate()', nextProps, nextState);
+    return true;
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    console.log('componentWillUpdate()', nextProps, nextState);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log('componentDidUpdate()', prevProps, prevState);
+  }
+
+  componentWillUnmount() {
+    console.log('componentWillUnmount()');
+  }
+
+  handleMakeOlder() {
+    console.log('handleMakeOlder()');
+    this.setState({
+      age: this.state.age + 1
+    })
+  }
+
+  handleMakeYounger() {
+    console.log('handleMakeYounger()');
+    this.setState({
+      age: this.state.age - 1
+    })
+  }
+
   render() {
-    return <div>Hello Steve Z!!</div>
+    console.log('render()');
+    return (
+      <div>
+        <h3>Name: {this.state.name}</h3>
+        <h3>Age: {this.state.age}</h3>
+        <button onClick={this.handleMakeOlder.bind(this)} >Make Older!</button>
+        <button onClick={this.handleMakeYounger.bind(this)} >Make Younger!</button>
+      </div>
+    )
   }
 }
 
