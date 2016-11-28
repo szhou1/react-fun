c88233d8b1312789ab12a5f3819ea9a563731374
szhou1 <zhousteve3@gmail.com>
2016-11-27 17:10:28 -0800

    (feat) Add other increment functions

--- src/components/counter/Counter.js
+++ src/components/counter/Counter.js
@@ -7,6 +7,17 @@ export default class Counter extends Component {
     onDecrement: PropTypes.func.isRequired
   }
 
+  incrementIfOdd = () => {
+    console.log(this)
+    if(this.props.value % 2 !== 0) {
+      this.props.onIncrement();
+    }
+  }
+
+  incrementAsync = () => {
+    setTimeout(this.props.onIncrement, 1000);
+  }
+
   render() {
     const { value, onIncrement, onDecrement } = this.props;
 
@@ -14,6 +25,8 @@ export default class Counter extends Component {
       <p> {value}
         <button onClick={onIncrement}> + </button>
         <button onClick={onDecrement}> - </button>
+        <button onClick={this.incrementIfOdd}> Increment If Odd </button>
+        <button onClick={this.incrementAsync}> Increment Async </button>
       </p>
     )
   }
